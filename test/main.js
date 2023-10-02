73886d52ca50e8117c80af78ad600d0ed20f8c2b
em2063 <em2063@hw.ac.uk>
2023-10-02 14:41:48 +0100

    changed javascript to jquery

--- test/main.js
+++ test/main.js
@@ -1,18 +1,24 @@
 //Reveals form when user clicks on shape
 function showForm(){
-    var form = document.querySelector("#square form");
-    form.style.display = "flex";
+    var form = $(".square form");
+    form.css("display", "flex");
 }
 
 //takes values from form to calculate area
-function area(form){
+function area(e){
+    e.preventDefault();
     var length = form.querySelector("#length").value;
     var width = form.querySelector("#width").value;
+    var areaLabel = document.getElementById("area-label");
 
     if(isNaN(length) || isNaN(width)){
         alert("Please enter valid number");
     }else{
         var area = length * width;
-        alert("The area is: " + area);
+        areaLabel.innerHTML = area;
     }
+
+    form.style.display = "none";
+    var square = document.querySelector(".square");
+    square.classList.add("square:hover");
 }
