193b685d11ce36f6ab29be9618c6d1c2948a3a3f
em2063 <em2063@hw.ac.uk>
2023-10-04 21:18:06 +0100

    made project section responsive to smaller screens

--- personal-website/index.js
+++ personal-website/index.js
@@ -1,1 +1,16 @@
+$(function() {
+    function responsive(){
+        var width = $(window).width(); //width of device
+        var element = $("#project-section") //get project containers
+        var projectContainers = $(".project-containers"); //get project elements
 
+        if(width < 768) {
+            element.css("flex-direction", "column") //change it to column for easier view
+            projectContainers.css("margin-top", "15px")
+        }else {
+            element.css("flex-direction", "row")
+        }
+    }
+    responsive(); //initailly call function
+    $(window).resize(responsive);
+})
