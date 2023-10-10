ca140ed62b1eec7988e5c4949bc8d30431978211
em2063 <em2063@hw.ac.uk>
2023-10-10 10:54:31 +0100

    added menu to view about section

--- personal-website/index.js
+++ personal-website/index.js
@@ -1,9 +1,15 @@
+    $(function() {
+        $('.desc-container').hide(); //Hide all containers originally
+        $('#skills-desc').show(); //originally show when page loads
+    });
+    
     function activeLink(linkElement){
         var tabs = $('.tab-links');
-        var tabDesc = $('.skills-list')
 
         tabs.removeClass('active-link'); //removes decoration from previous
         $(linkElement).addClass('active-link') //Adds decorstion to active link
-        tabDesc.removeClass('activeList'); //Hide last description
 
+        $('.desc-container').hide();
+        var id  = $(linkElement).text().toLowerCase() + '-desc'; //get container based off tab pressed
+        $('#' + id).show(); //show requiered container
     }
