8cd0068050e42f1a89adbc1f90bf7a29578b043d
em2063 <em2063@hw.ac.uk>
2023-12-19 10:58:24 +0000

    added UI updates for quiz feature

--- flashcard-app/flashcard-app-ui/quiz.js
+++ flashcard-app/flashcard-app-ui/quiz.js
@@ -1,8 +1,13 @@
 //array to store folders
 folders = [];
+//array to store flashcards
+flashcards = [];
 
-//get folder list
+//folder elements
 folderList = document.getElementById("folder-list");
+
+//flashcard elements
+flashcardGrid = document.getElementById("quiz-grid");
 
 //gets folders from server
 const fetchFolders = async () => {
@@ -16,7 +21,50 @@ const fetchFolders = async () => {
   }
 };
 
+const fetchFlashcards = async () => {
+  try {
+    const response = await fetch("http://localhost:5000/api/flashcards");
+    const flashcard = await response.json();
+    flashcards = flashcard;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
 fetchFolders();
+fetchFlashcards();
+
+function startQuiz(folderId) {
+  flashcardGrid.innerHTML = "";
+
+  if (flashcards.length > 0) {
+    const flashcardItem = document.createElement("div");
+    flashcardItem.classList.add("flashcard-quiz-item");
+
+    const flashcardContent = document.createElement("div");
+    flashcardContent.classList.add("flashcard-text-container");
+
+    const flashcardText = document.createElement("div");
+    flashcardText.classList.add("flashcard-text");
+
+    const currentFolder = flashcards.filter(
+      (flashcard) => flashcard.folderId !== folderId
+    );
+    const firstFlashcard = currentFolder[0];
+
+    if (!firstFlashcard.isRevealed) {
+      const flashcardTitle = document.createElement("h2");
+      flashcardText.appendChild(flashcardTitle);
+      flashcardTitle.textContent = firstFlashcard.title;
+    } else {
+      flashcardText.textContent = firstFlashcard.content;
+    }
+
+    flashcardGrid.appendChild(flashcardItem);
+    flashcardContent.appendChild(flashcardText);
+    flashcardItem.appendChild(flashcardContent);
+  }
+}
 
 function createFolder() {
   folderList.innerHTML = "";
@@ -25,6 +73,9 @@ function createFolder() {
     folderItem.classList.add("folder-item");
     const buttonText = document.createElement("p");
     buttonText.innerHTML = folder.title;
+    folderItem.addEventListener("click", () => {
+      startQuiz(folder.id);
+    });
 
     const buttonContainer = document.createElement("div");
     buttonContainer.classList.add("flashcard-header");
